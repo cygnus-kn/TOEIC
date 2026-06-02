45ebf8613637ccdd6cab6bd873f8efff8b0293eb
cygnus-kn <hiepbq14408@gmail.com>
2026-06-02 20:46:04 +0700

    Add S141 HW07 (02/06) homework and update sidebar manifest

--- js/data.js
+++ js/data.js
@@ -5,6 +5,7 @@
 const CLASSES_DATA = {
   S141: {
     homework: [
+      { date: "[HW07] 02/06" },
       { date: "[HW06] 30/05" },
       { date: "[HW05] 28/05" },
       { date: "[HW04] 26/05" },
