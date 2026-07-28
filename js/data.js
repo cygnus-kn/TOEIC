45c3b94bf7a75a9b5fc42435dc06a26ae07e2526
cygnus-kn <hiepbq14408@gmail.com>
2026-07-28 22:42:47 +0700

    feat: add HW19 data entry and associated JSON configuration file for class S139

--- js/data.js
+++ js/data.js
@@ -5,6 +5,7 @@
 const CLASSES_DATA = {
   S139: {
     homework: [
+      { date: "[HW19] 28/07" },
       { date: "[HW18] 25/07" },
       { date: "[HW17] 23/07" },
       { date: "[HW16] 21/07" },
