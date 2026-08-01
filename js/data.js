cd557a1286ecea9762664920084e48f5bad04392
cygnus-kn <hiepbq14408@gmail.com>
2026-08-01 22:46:28 +0700

    feat: add new homework assignments [HW21] and [HW31] to class data and create corresponding task files

--- js/data.js
+++ js/data.js
@@ -5,6 +5,7 @@
 const CLASSES_DATA = {
   S139: {
     homework: [
+      { date: "[HW21] 01/08" },
       { date: "[HW20] 30/07" },
       { date: "[HW19] 28/07" },
       { date: "[HW18] 25/07" },
@@ -30,6 +31,7 @@ const CLASSES_DATA = {
   },
   S141: {
     homework: [
+      { date: "[HW31] 01/08" },
       { date: "[HW30] 25/07" },
       { date: "[HW29] 23/07" },
       { date: "[HW28] 21/07" },
